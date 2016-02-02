14b6acc2d6726bb6b1caf4da3f923e52c00aa1e8
Ondrej Kubik <ondrej.kubik@canonical.com>
2016-02-02 16:27:39 +0000

    Adding support for optional device blobs in device package
    When device blobs folder is suplied, device blobs will be unpack into it
    
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>

--- unpack_rootfs.sh
+++ unpack_rootfs.sh
@@ -25,8 +25,21 @@ if [[ ! -z $UBUNTU_ROOTFS_PACKAGES ]]; then
     do
         if [ -f $package ]; then
             echo "Unpacking $package"
-            tar --numeric-owner --strip-components=1 --exclude=system/dev/* -xvvf $package --directory $1 >> $1.content
+            tar --numeric-owner --strip-components=1 --exclude=system/dev/* -xvvf $package --directory $1 --wildcards system/ >> $1.content
         fi
     done
+
+    # echo unpack device blobs if needed
+    if [ -n "$3" ]; then
+        rm -rf $3
+        mkdir -p $3
+        for package in $UBUNTU_ROOTFS_PACKAGES
+        do
+            if [ -f $package ]; then
+                echo "Unpacking blobs from $package"
+                tar --strip-components=1 --exclude=system/* -xvvf $package --directory $3
+            fi
+        done
+    fi
 fi
 
